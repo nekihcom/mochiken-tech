9aae4e068acb635969698b1a63abd79ff0859121
nekihcom <kmmch.tech@gmail.com>
2025-01-31 22:24:38 +0900

    create drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Heading, Icon } from "@chakra-ui/react";
 
 import PrimaryContainer from "./PrimaryContainer";
 import { FcMenu } from "react-icons/fc";
+import HeaderDrawer from "./HeaderDrawer";
 
 const Header = () => {
   return (
@@ -11,9 +12,10 @@ const Header = () => {
           <Flex justify={'space-between'} css={{py:3}}>
             <Flex align={'center'} ><Heading>Mochiken</Heading></Flex>
             <Box>
-              <Icon fontSize="40px" color="tomato">
+              {/* <Icon fontSize="40px" color="tomato">
                 <FcMenu />
-              </Icon>
+              </Icon> */}
+              <HeaderDrawer />
             </Box>
           </Flex>
         </PrimaryContainer>
