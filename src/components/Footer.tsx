9e23a548ab5b15b759c5e397c8efd86d4086f970
nekihcom <kmmch.tech@gmail.com>
2025-02-01 16:22:01 +0900

    create footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,37 @@
 import Link from 'next/link';
 
+import { Box, Flex } from '@chakra-ui/react';
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import Image from 'next/image';
+
 const Footer = () => {
   return (
     <>
-      <div className='py-10 px-5'>
-        <p className='text-center'>&copy; Copyright {new Date().getFullYear()}. Made by <Link href='/'>Masaya Kemmochi</Link></p>
-      </div>
+      <Box css={{py:10, px:5}} className='py-10 px-5'>
+        <Box css={{margin:'10px auto', maxW:'50%'}}>
+          <Flex justify={'space-between'}>
+            <Box>
+              <Link href={'https://x.com/nekihcom'} target='_blank'>
+                <FaXTwitter size={'2rem'} />
+              </Link>
+            </Box>
+            <Box>
+              <Link href={'https://github.com/nekihcom'} target='_blank'>
+                <FaGithub size={'2rem'} />
+              </Link>
+            </Box>
+            <Box>
+              <Link href={'https://qiita.com/nekihcom'} target='_blank'>
+                <Image src={'/icon/qiita-green.png'} alt={"Qiita"} width={32} height={32} />
+              </Link>
+            </Box>
+          </Flex>
+        </Box>
+        <Box css={{textAlign:'center'}}>
+          &copy; Copyright {new Date().getFullYear()}. Made by <Link href='/'>Masaya Kemmochi</Link>
+        </Box>
+      </Box>
     </>
   );
 }
