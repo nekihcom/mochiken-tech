4a52609cfc8278e534490fab01dc21d0d3fee50b
nekihcom <kmmch.tech@gmail.com>
2025-02-02 13:51:17 +0900

    modify default theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from './color-mode'
-import { system } from '@/config/theme'
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
